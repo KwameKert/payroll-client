7d5070a736243cd000090883c816c2d372218d7d
kwamekert <kwamekert@gmail.com>
2022-03-17 23:07:54 +0000

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
